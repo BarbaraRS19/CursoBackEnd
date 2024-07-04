b18574ae70deecc52688c98881983e0c66f49264
BÁRBARA ROSA SABINO <barbara_sabino@estudante.sesisenai.org.br>
2024-07-04 17:35:37 -0300

    cursobe

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const express = require('express')
+const {Client} = require('pg')
+require("dotenv").config()
+
+const client = new Client({
+    host: process.env.host,
+    port: process.env.post,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+})
 
 const app = express()
+
+const connectDB = async () => {
+    client.connect()
+    .then(() => {
+        console.log('A conexão funcionou!')
+    }).catch((err) => {
+        console.error('Erro ao conectar!')
+    });
+};
+connectDB()
 
 app.get('/test-api', function (req, res){
     res.send('NOSSA API ESTÁ FUNCIONANDO')
