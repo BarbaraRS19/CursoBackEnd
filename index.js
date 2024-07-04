5e954e05e2acc2a2c0e04d03ccb433b86f8529c2
BÁRBARA ROSA SABINO <barbara_sabino@estudante.sesisenai.org.br>
2024-07-04 16:29:54 -0300

    backend

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-function filtrarPares(arr){
-    nova_arr = []
-    for(let i = 0; i < arr.length; i++){
-        if (arr[i] % 2 === 0){
-            nova_arr.push(arr[i])
-        }
-    }
-   return nova_arr
-}
+const express = require('express')
+
+const app = express()
+
+app.get('/test-api', function (req, res){
+    res.send('NOSSA API ESTÁ FUNCIONANDO')
+})
+app.listen(8000)
